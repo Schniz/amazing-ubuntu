5db3fe298f6afe5978bd07353f99ae54f4c12d06
Gal Schlezinger <gal@spitfire.co.il>
2014-10-25 01:44:21 -0700

    installing oh my zsh and pulling my aliases

--- setup.sh
+++ setup.sh
@@ -10,4 +10,9 @@ sudo add-apt-repository ppa:wei-qianghb/mutate
 sudo apt-get update
 sudo apt-get install mutate
 
+echo "Installing oh my zsh"
+curl -L http://install.ohmyz.sh | sh
+
 # TODO: download my latest .zshrc, aliases and stuff
+\curl https://raw.githubusercontent.com/Schniz/amazing-ubuntu/master/.zshrc > $HOME/.zshrc
+\curl https://raw.githubusercontent.com/Schniz/amazing-ubuntu/master/.aliases > $HOME/.aliases
